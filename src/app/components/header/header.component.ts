a67b34361a5363f06b520eee8e6fdf0271d0a123
Marco Scherf <scheco13@gmail.com>
2023-01-16 19:57:44 +0100

    changed user menu in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 import { SidenavService } from 'src/app/services/sidenav.service';
 
@@ -10,10 +12,24 @@ import { SidenavService } from 'src/app/services/sidenav.service';
 })
 export class HeaderComponent implements OnInit {
   @Input() auth: boolean;
-  constructor(private sidennavService: SidenavService, public Auth: Auth) {}
+  constructor(
+    private sidennavService: SidenavService,
+    public Auth: Auth,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
   close() {
     this.sidennavService.toggle();
+  }
+
+  logout() {
+    if (this.Auth.currentUser.isAnonymous) {
+      this.authService.deleteUsers();
+    }
+    this.authService.logout().then(() => {
+      this.router.navigate(['login']);
+    });
   }
 }
